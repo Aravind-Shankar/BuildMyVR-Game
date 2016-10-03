650a72accc5f19b3bd62d17133f3c759229a7c17
Jason Stanley <js061097@Jasons-MacBook-Pro.local>
2016-10-04 04:50:54 +0530

    micro world

---
+++ Assets/BoxFocus.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxFocus : MonoBehaviour {
+
+
+
+	public Renderer rend;
+	public bool isDefect;
+	// Use this for initialization
+	void Start () {
+
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Focus()
+	{
+		
+		rend.material.color = new Color (0f, 100f, 0f, 0f);
+		if (isDefect) {
+			rend.material.color = new Color (100f, 0f, 0f, 0f);
+		}
+	}
+	public void Ignore()
+	{
+		rend.material.color = Color.white; 
+
+	}
+
+}
