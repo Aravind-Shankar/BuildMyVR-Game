ba5412ba66c4d64423dfcdef60b76239c94f8fb8
Jason Stanley <js061097@Jasons-MacBook-Pro.local>
2016-10-12 14:43:20 +0530

    speedometer

---
+++ Assets/Scripts/Macro/Speedometer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Macro
+{
+	
+	public class Speedometer : MonoBehaviour {
+		public Rigidbody target;
+		private Text speedText;
+		// Use this for initialization
+		void Start () {
+			
+			speedText=GetComponent<Text>();
+		}
+		
+		// Update is called once per frame
+		void FixedUpdate () {
+			
+		 	double speed = target.velocity.magnitude;
+		 	speed = speed * (5.0 / 18.0);
+		 	speedText.text = speed + " kmph";
+		}
+	}
+
+}
