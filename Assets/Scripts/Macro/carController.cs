8c3ece82d9ecd2140e22d7c118390b3523c011bb
karthi2107 <karthikeswaren1@gmail.com>
2016-12-05 18:51:30 +0530

    Acc defect working

--- Assets/Scripts/Macro/carController.cs
+++ Assets/Scripts/Macro/carController.cs
@@ -2,99 +2,107 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-namespace Macro
-{
 
-	public class carController : MonoBehaviour {
 
-		public WheelCollider wheelFL;
-		public WheelCollider wheelFR;
-		public WheelCollider wheelRL;
-		public WheelCollider wheelRR;
+public class carController : MonoBehaviour {
 
-		private Rigidbody rb;
+	public WheelCollider wheelFL;
+	public WheelCollider wheelFR;
+	public WheelCollider wheelRL;
+	public WheelCollider wheelRR;
 
-		public float maxSteer = 20.0f;
-		public float maxFBrake = 50.0f;
-		public float maxRBrake = 10.0f;
 
-		public float maxTorque = 1000.0f;
-		public float acceleration = 0.2f;
-		private float accFactor = 0.0f;
+	private Rigidbody rb;
 
-		private float triggerTime = 0.0f; //time after which the trigger has been pulled
-		private bool triggerTimer; //says whether the timer is on or off
-		public float triggerCutoffTime = 1.0f; //time interval for the double click. (triggers the scene change)
+	public float maxSteer = 20.0f;
+	public float maxFBrake = 30.0f;
+	public float maxRBrake = 10.0f;
 
-		// Use this for initialization
-		void Start () {
-			rb = GetComponent<Rigidbody> ();
-			rb.centerOfMass = new Vector3 (0.0f, -0.9f, 0.0f);
 
-			LoadMacroState ();
+	public float maxTorque = 1000.0f;
+	private float acceleration = 100.0f;
+	private float accFactor = 0.0f;
+	public bool isAccDefect;
+
+	private float triggerTime = 0.0f; //time after which the trigger has been pulled
+	private bool triggerTimer; //says whether the timer is on or off
+	public float triggerCutoffTime = 1.0f; //time interval for the double click. (triggers the scene change)
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.centerOfMass = new Vector3 (0.0f, -0.9f, 0.0f);
+
+		LoadMacroState ();
+	}
+
+	/*
+	void OnGUI(){
+		GUI.Label (new Rect (10.0f, 10.0f, 50.0f, 50.0f), "Acc Defective: " + isAccDefect);
+	}
+	*/
+	void OnEnable() {
+		MagnetSensor.OnCardboardTrigger += CarBrakes;
+	}
+
+	void OnDisable() {
+		MagnetSensor.OnCardboardTrigger -= CarBrakes;
+	}
+
+	void FixedUpdate () {
+		//used to inc the torque linearly
+		if (accFactor <= 1) {
+			accFactor += acceleration * (Time.deltaTime);
+		}
+		AccDefect ();
+
+		wheelRL.motorTorque = -accFactor * maxTorque;
+		wheelRR.motorTorque = -accFactor * maxTorque;
+		
+		//linearly inc the steering angle using accelerometer
+		if (Mathf.Abs (Input.acceleration.x) > 1.0f) {
+			wheelFL.steerAngle = maxSteer;
+			wheelFR.steerAngle = maxSteer;
+		} 
+
+		else {
+			wheelFL.steerAngle = maxSteer * Input.acceleration.x;
+			wheelFR.steerAngle = maxSteer * Input.acceleration.x;
 		}
 
-		void OnEnable() {
-			MagnetSensor.OnCardboardTrigger += CarBrakes;
-		}
-
-		void OnDisable() {
-			MagnetSensor.OnCardboardTrigger -= CarBrakes;
-		}
-
-		void FixedUpdate () {
-			//used to inc the torque linearly
-			if (accFactor <= 1) {
-				accFactor += acceleration * (Time.deltaTime);
-			}
-
-			wheelRL.motorTorque = -accFactor * maxTorque;
-			wheelRR.motorTorque = -accFactor * maxTorque;
-
-			//linearly inc the steering angle using accelerometer
-			if (Mathf.Abs (Input.acceleration.x) > 1.0f) {
-				wheelFL.steerAngle = maxSteer;
-				wheelFR.steerAngle = maxSteer;
-			} 
-
-			else {
-				wheelFL.steerAngle = maxSteer * Input.acceleration.x;
-				wheelFR.steerAngle = maxSteer * Input.acceleration.x;
-			}
-
-			if (Input.GetKeyDown (KeyCode.Space))
-				CarBrakes ();
+		if (Input.GetKeyDown (KeyCode.Space))
+			CarBrakes ();
 				
-			if (triggerTimer == true) {
-				triggerTime += Time.deltaTime;
-			} 
+		if (triggerTimer == true) {
+			triggerTime += Time.deltaTime;
+		} 
 
-			else {
-				triggerTime = 0.0f;		
-			}
-
-			if (sceneManager.instance.inSceneTransition)
-				SaveMacroState ();
+		else {
+			triggerTime = 0.0f;		
 		}
 
-		void LoadMacroState() {
-			MacroState pastState = sceneManager.instance.GlobalMacroState;
-			if (pastState != null) {
-				transform.position = pastState.carPosition;
-				transform.rotation = pastState.carRotation;
-			}
+		if (sceneManager.instance.inSceneTransition)
+			SaveMacroState ();
+	}
+
+	void LoadMacroState() {
+		MacroState pastState = sceneManager.instance.GlobalMacroState;
+		if (pastState != null) {
+			transform.position = pastState.carPosition;
+			transform.rotation = pastState.carRotation;
 		}
+	}
 	
-		void SaveMacroState() {
-			MacroState globalState = sceneManager.instance.GlobalMacroState;
-			if (globalState == null)
-				globalState = sceneManager.instance.GlobalMacroState = new MacroState ();
+	void SaveMacroState() {
+		MacroState globalState = sceneManager.instance.GlobalMacroState;
+		if (globalState == null)
+			globalState = sceneManager.instance.GlobalMacroState = new MacroState ();
 			
-			globalState.carPosition = transform.position;
-			globalState.carRotation = transform.rotation;
-		}
+		globalState.carPosition = transform.position;
+		globalState.carRotation = transform.rotation;
+	}
 
-		void CarBrakes(){
+	void CarBrakes(){
 			/*if (triggerTime == 0.0f) {
 				wheelFL.brakeTorque = maxFBrake; 
 				wheelFR.brakeTorque = maxFBrake;
@@ -111,10 +119,19 @@ namespace Macro
 				triggerTime = 0.0f;
 				triggerTimer = false;
 			}*/
-			wheelFL.brakeTorque = maxFBrake; 
-			wheelFR.brakeTorque = maxFBrake;
-			wheelRR.brakeTorque = maxRBrake;
-			wheelRL.brakeTorque = maxRBrake;
+		wheelFL.brakeTorque = maxFBrake; 
+		wheelFR.brakeTorque = maxFBrake;
+		wheelRR.brakeTorque = maxRBrake;
+		wheelRL.brakeTorque = maxRBrake;
+	}
+
+	public void AccDefect(){
+		if (isAccDefect == true){
+			accFactor -= acceleration * Time.deltaTime; 
+			if (accFactor < 0.0f) {
+				accFactor = 0.0f;
+			}
 		}
 	}
+
 }
--- Assets/Scripts/Managers/defectManager.cs
+++ Assets/Scripts/Managers/defectManager.cs
@@ -1,28 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class defectManager : MonoBehaviour {
 
-	private float defectTime;
+	private float speedometerDefectTime;
+	private float accelerationDefectTime;
 	public GameObject car;
-	private carSpeedometer c;
+	private carSpeedometer s;
+	private carController c;
+	private Random r;
+	private static int l;
+							
 	// Use this for initialization
 	void Start () {
-		defectTime = 10.0f;
-		c = car.GetComponent<carSpeedometer> ();
-		defectTime += (10.0f) * (Random.value);
-		print (defectTime);
+		
+		speedometerDefectTime = 10.0f * nonRepRandom (1, 5);
+		s = car.GetComponent<carSpeedometer> ();
+		speedometerDefectTime += (10.0f) * (Random.value);
 
+		print (speedometerDefectTime);
+
+		accelerationDefectTime = 10.0f * nonRepRandom (1, 5);
+		c = car.GetComponent<carController> ();
+		accelerationDefectTime += (10.0f) * (Random.value);
+
+		print (accelerationDefectTime);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		defectTime -= Time.deltaTime;
+		speedometerDefectTime -= Time.deltaTime;
 
-		if (defectTime <= 0.0f) {
-			c.isDefect = true;
+		if (speedometerDefectTime <= 0.0f) {
+			s.isDefect = true;
 		}
+		else if (accelerationDefectTime <= 0.0f) {
+			c.isAccDefect = true;
+		}
+	}
 
+	public static int nonRepRandom(int min, int max){
+		int a = Random.Range (min, max);
 
+		if (a == l) {
+			return nonRepRandom (min, max);
+		}
+		l = a;
+		return a;
 	}
 }
