10dd6a7e84984f940ca341e3640e84954e7c5ec4
--get <shankaravind167@gmail.com>
2016-12-12 11:31:36 +0530

    Added dummy cam controller to show a static non-VR camera before network starts.

---
+++ Assets/Scripts/General/DummyCamController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class DummyCamController : MonoBehaviour {
+
+	private Camera attachedCamera;
+
+	void OnEnable() {
+		attachedCamera = GetComponent<Camera> ();
+	}
+
+	void Update() {
+		if (NetworkManager.singleton != null) {
+			attachedCamera.enabled = !NetworkManager.singleton.isNetworkActive;
+			attachedCamera.tag = (attachedCamera.enabled) ? "MainCamera" : "Untagged";
+		}
+	}
+}
