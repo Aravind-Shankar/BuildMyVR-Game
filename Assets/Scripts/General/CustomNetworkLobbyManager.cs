87066db1280a73a1b099a87bf9aef587bd4740d5
--get <shankaravind167@gmail.com>
2016-12-16 08:50:44 +0530

    Added bare-bones scripts for managing metadata between lobby and game

---
+++ Assets/Scripts/General/CustomNetworkLobbyManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class CustomNetworkLobbyManager : NetworkLobbyManager {
+
+	public override bool OnLobbyServerSceneLoadedForPlayer(
+		GameObject lobbyPlayer, GameObject gamePlayer)
+	{
+		LobbyPlayer lobbyPlayerComponent = lobbyPlayer.GetComponent<LobbyPlayer> ();
+		CarPlayer carPlayerComponent = gamePlayer.GetComponent<CarPlayer> ();
+
+		return true;
+	}
+
+}
