3145529b984d6b789c5d46f0655d18c093de1f93
RamAnanth <ram.ananth14@gmail.com>
2016-12-19 16:36:43 +0530

    Tried connecting using name instead of IP

---
+++ Assets/Scripts/General/CustomNetworkDiscovery.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+    private string dataString, addressString;
+   
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        dataString = data;
+        addressString = fromAddress;
+        
+    }
+
+    void OnGUI() {
+        dataString = GUI.TextField(new Rect(10, 10, 200, 20),dataString, 25);
+        if (GUI.Button(new Rect(10, 70, 200, 20), "Connect"))
+        {
+            NetworkManager.singleton.networkAddress = addressString;
+            NetworkManager.singleton.StartClient();
+        }
+            
+    }
+}
