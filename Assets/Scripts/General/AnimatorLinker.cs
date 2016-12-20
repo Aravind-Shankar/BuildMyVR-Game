8eac22bd627b26404864338bf656f6ccad6faf2d
--get <shankaravind167@gmail.com>
2016-12-20 13:02:02 +0530

    Added some music files and set up for scene animation in splash screen

---
+++ Assets/Scripts/General/AnimatorLinker.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorLinker : MonoBehaviour {
+	public Animator[] linkedAnimators;
+
+	public void StartLinkedAnimator(int nextAnimatorIndex) {
+		linkedAnimators [nextAnimatorIndex].SetTrigger ("startPlaying");
+	}
+}
