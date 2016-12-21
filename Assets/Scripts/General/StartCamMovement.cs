e7fe72387e09928f67ae3478592de140810beb9e
--get <shankaravind167@gmail.com>
2016-12-21 08:17:12 +0530

    Finished the rest of splash screen animations using iTween library, now need to do GUI

---
+++ Assets/Scripts/General/StartCamMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartCamMovement : MonoBehaviour {
+
+	public Camera camera;
+	public string camPathName;
+	public float camMoveTime;
+
+	public GameObject carVR;
+	public string carPathName;
+	public float carMoveTime;
+
+	public float intervalTime;
+
+	public void StartMovement() {
+		this.camera.clearFlags = CameraClearFlags.Skybox;
+		iTween.MoveTo (this.camera.gameObject, iTween.Hash (
+			"path", iTweenPath.GetPath(this.camPathName),
+			"time", camMoveTime,
+			"easeType", iTween.EaseType.easeInOutSine
+		));
+		StartCoroutine (WaitAndStartCar());
+	}
+
+	IEnumerator WaitAndStartCar() {
+		yield return new WaitForSeconds(intervalTime);
+		iTween.MoveTo(carVR, iTween.Hash(
+			"path", iTweenPath.GetPath(this.carPathName),
+			"time", carMoveTime
+		));
+		yield return null;
+	}
+}
