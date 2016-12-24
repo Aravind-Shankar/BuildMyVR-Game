c0a96a25d22cd97477e2c1f6a50089b96ef99bda
--get <shankaravind167@gmail.com>
2016-12-24 22:55:16 +0530

    Visually done with marking players READY.

---
+++ Assets/Scripts/General/VRPrepHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VRPrepHandler : MonoBehaviour {
+
+	private GameObject magnetNotice, successText;
+
+	void Awake() {
+		magnetNotice = transform.FindChild ("Magnet Notice").gameObject;
+		successText = transform.FindChild ("Success Text").gameObject;
+	}
+
+	void OnEnable() {
+		MagnetSensor.OnCardboardTrigger += MarkReady;
+		magnetNotice.SetActive (true);
+		successText.SetActive (false);
+	}
+
+	#if UNITY_EDITOR
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space))
+			MarkReady ();
+	}
+	#endif
+
+	public void MarkReady() {
+		magnetNotice.SetActive (false);
+		successText.SetActive (true);
+	}
+
+	public void GoBack() {
+		magnetNotice.SetActive (true);
+		successText.SetActive (false);
+	}
+
+	void OnDisable() {
+		MagnetSensor.OnCardboardTrigger -= MarkReady;
+	}
+
+}
