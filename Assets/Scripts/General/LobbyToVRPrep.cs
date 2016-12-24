bfb34a42b472ba73ae5b5be9b35bf5f212b07fee
--get <shankaravind167@gmail.com>
2016-12-24 22:37:16 +0530

    Transition to VR done

---
+++ Assets/Scripts/General/LobbyToVRPrep.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LobbyToVRPrep : MonoBehaviour {
+
+	public GameObject propCar;
+
+	public void TransitionToVR() {
+		gameObject.SetActive (false);
+		Camera.main.enabled = false;
+
+		GameObject gvrMain = propCar.transform.FindChild ("GvrMain").gameObject;
+		gvrMain.SetActive (true);
+	}
+
+}
