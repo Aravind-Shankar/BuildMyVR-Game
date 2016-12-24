5c0c02ded82953578be19a0f41a1b90e599b6abd
--get <shankaravind167@gmail.com>
2016-12-24 23:33:39 +0530

    Visually done with all forward transitions from splash.

---
+++ Assets/Scripts/General/MainMenuHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuHandler : MonoBehaviour {
+
+	public GameObject gameFinderCanvas;
+
+	public void HitPlay() {
+		gameObject.SetActive (false);
+		gameFinderCanvas.SetActive (true);
+	}
+
+	public void HitQuit() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+		Application.Quit ();
+	}
+
+}
