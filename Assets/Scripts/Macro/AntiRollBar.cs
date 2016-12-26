c6de1ef16605bb7908b8752ff116e5df344c900e
--get <shankaravind167@gmail.com>
2016-12-26 15:43:42 +0530

    AntiRollBars help stability of the car. Guess we can mark the skid issua as solved.

---
+++ Assets/Scripts/Macro/AntiRollBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AntiRollBar : MonoBehaviour {
+
+	public WheelCollider wheelL;
+	public WheelCollider wheelR;
+	public float antiRollFactor = 5000f;
+
+	private Rigidbody attachedRigidbody;
+
+	void Start() {
+		attachedRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		WheelHit hit = new WheelHit ();
+		float travelL = 1f, travelR = 1f;
+
+		bool groundedL = wheelL.GetGroundHit (out hit);
+		if (groundedL)
+			travelL = (-wheelL.transform.InverseTransformPoint (hit.point).y - wheelL.radius) /
+						wheelL.suspensionDistance;
+
+		bool groundedR = wheelR.GetGroundHit (out hit);
+		if (groundedR)
+			travelR = (-wheelR.transform.InverseTransformPoint (hit.point).y - wheelR.radius) /
+						wheelR.suspensionDistance;
+
+		float antiRollForce = (travelL - travelR) * antiRollFactor;
+		if (groundedL)
+			attachedRigidbody.AddForceAtPosition (wheelL.transform.up * -antiRollForce,
+				wheelL.transform.position);
+		if (groundedR)
+			attachedRigidbody.AddForceAtPosition (wheelR.transform.up * antiRollForce,
+				wheelR.transform.position);
+	}
+
+}
